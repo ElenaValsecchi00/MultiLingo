8cb158bd4cb9456d1245047840ef7919118876fc
ElenaValsecchi00 <elena.valsecchi12@gmail.com>
2024-04-29 17:29:10 +0200

    multilingual plugin

--- MultiLingo/vite.config.js
+++ MultiLingo/vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +13,7 @@ export default defineConfig({
     vue(),
     vueJsx(),
     VueDevTools(),
+    VueI18nPlugin()
   ],
   resolve: {
     alias: {
