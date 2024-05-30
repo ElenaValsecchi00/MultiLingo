60aa72aae5a003ceaccc1038c1123a68b3cc01b6
ElenaValsecchi00 <elena.valsecchi12@gmail.com>
2024-05-30 15:21:41 +0200

    Molto carino
    Co-authored-by: nunzia-damb <nunzia-damb@users.noreply.github.com>

--- MultiLingo/src/components/Lev3.vue
+++ MultiLingo/src/components/Lev3.vue
@@ -12,24 +12,26 @@
         </header>
         
         <p>{{ $t("assignment.header_1_3") }}</p>
-        
-        <h3 class="text">{{ $t("assignment.header_3") }}</h3>
-        
-        <form>
-
-        <div >
-            <p class="textanswer">{{this.message}}</p>
+        <div class="scrollable"> 
+            <h3 class="text">{{ $t("assignment.header_3") }}</h3>
+            <div v-for="(name, index) in this.conversation">
+                <h3 class="text" :class="{ 'answer': index % 2 == 0}">{{ name }}</h3> 
         </div>
-        <a href="#" v-on:click="getNextQuestion()" id="submit">SUBMIT</a>
-        </form>
-         <!--When pressed first time starts recording, when pressed second time stops-->
-         <button class="buttonAudio" :disabled="disabledMic" @click="startRecordAudio" :class="{'clickable': recording}">
+             
+        </div>
+        
+        <div class="message">
+            <form>
+            <p class="textanswer" >{{this.message}}</p>
+            <a href="#" v-on:click="getNextQuestion()" id="submit">SUBMIT</a>
+            </form>
+        </div>
+        <!--When pressed first time starts recording, when pressed second time stops-->
+        <button class="buttonAudio" @click="startRecordAudio()" :class="{'clickable': recording}">
             <img  class="audioImg"  
             :src="recording ? imageRecording : imageNotRecording">
         </button>
-        <h3 class="text" :class="{ 'hide': !seen }">{{ question }}</h3>
-        <button class="buttonConferma" @click="get_result">{{ $t("assignment.confirm") }}</button>
-      
+        
     </body>
 </template>
 
@@ -51,12 +53,14 @@ export default {
             imageRecording: '../../micro/listening.png',
             recorder:null,
             disabledMic: false,
+            conversation: null 
         };
     },
     created(){
         this.flag = this.$route.params.language;
         this.src = '../../flags/' + this.flag + ".png";
         this.language = i18n.global.locale.value;
+        this.conversation = [];
     },
     mounted() {
         
@@ -66,11 +70,12 @@ export default {
             router.go(-1);
         },
         getNextQuestion(){
+            this.conversation.push(this.message)
             axios.post('http://127.0.0.1:5000/lev3/conversation', {data: this.message})
             .then(response => { 
                 console.log(response.data)
-                this.question = response.data
-                this.seen = true
+                this.conversation.push(response.data)
+                router.go(-1);
             })
             .catch(error => {
                 console.log(error)
@@ -92,8 +97,8 @@ export default {
             axios.get('http://127.0.0.1:5000/lev3/conversation')
             .then(response => { 
                 console.log(response.data)
-                this.message = response.data
-                this.disabledConfirm = false
+                this.disabledConfirm = false;
+                this.message = response.data;
             })
             .catch(error => {
                 console.log(error)
@@ -108,71 +113,21 @@ body{
     background-color: #FBF2D4;
 }
 
-.options{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-}
-
-.buttonConferma {
-    position:absolute;
-    width: 50%;
-    height: 40px;
-    text-align: center;
-    left: 0; 
-    right: 0; 
-    bottom:40px;
-    margin-left: auto; 
-    margin-right: auto; 
-    background: #C3E986;
-    border-radius: 10px;
-    border-color: transparent;
-    cursor: pointer;
-}
-
 #submit{
-    text-align: center;
-    padding:150px;
+    text-align: right;
+    position: relative;
+    left: 250px;
+    bottom:45px;
 }
 
-.buttonConferma:disabled{
-    background-color: rgba(172, 160, 160, 0.806);
-    cursor: not-allowed;
-}
-
-.buttonAudio{
-    position:relative;
-    width: 10%;
-    height: auto;
-    text-align: center;
-    left: 0; 
-    right: 0; 
-    margin-left: auto; 
-    margin-right: auto; 
-    background: #C3E986;
-    border-radius: 10px;
-    border-color: transparent;
-    cursor: pointer;
-}
 .audioImg{
-    width:50%;
+    width:30%;
+}
+.scrollable{
+    height:400px;
+    overflow: auto;
 }
 
-.clickable-div {
-  padding: 5px;
-  margin: 5px;
-  border-radius: 10px;
-  background-color: rgb(188, 224, 149);
-  cursor: pointer;
-  transition: all 0.3s ease;
-}
-
-
-.clickable {
-    background-color: rgb(6, 148, 6);
-    transform: scale(0.95);
-    transition: transform 0.5s ease;
-}
 
 .container{
     position:relative;
@@ -188,7 +143,6 @@ body{
     margin-right: 1cap;
 }
 
-
 .text{
     width: auto;
     height: auto;
@@ -200,21 +154,14 @@ body{
 }
 
 .textanswer{
-    width: auto;
-    height: 7vh;
-    background-color: white;
-    font-size: smaller;
-    text-align: center;
-    border-radius: 10px;
-    font-size: 20px;
-    margin: 20px;
-    padding: 10px;
+    font-size: small;
+    text-align: left;
+    width: 250px;
+    height: 70px;
 }
-
-
-.buttonAudio:disabled{
-    background-color: rgba(172, 160, 160, 0.806);
-    cursor: not-allowed;
+.answer{
+    background-color: white;
+    position:left;
 }
 .buttonAudio{
     position:absolute;
@@ -223,7 +170,7 @@ body{
     text-align: center;
     left: 0; 
     right: 0; 
-    bottom:100px;
+    bottom:40px;
     margin-left: auto; 
     margin-right: auto; 
     background: #C3E986;
@@ -231,12 +178,29 @@ body{
     border-color: transparent;
     cursor: pointer;
 }
+.clickable {
+    background-color: rgb(6, 148, 6);
+    transform: scale(0.95);
+    transition: transform 0.5s ease;
+}
+
 .audioImg{
     width:50%;
 }
 
 .hide{
     visibility: hidden !important;
+}
+
+.message{
+    background-color: white;
+    border-radius: 10px;
+    width: 315px;
+    height: 70px;
+    margin: 20px;
+    position:absolute;
+    bottom: 130px;
+    display: inline-flex;
 }
 
 </style>
